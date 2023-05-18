a5d0eaec0894e1199548bbecca6b002eb994a3f8
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-05-18 14:40:53 +0000

    Primeiro desafio

---
+++ .ccls-cache/@home@runner@Desafio-1-100-primeiros-numeros-naturais/00000 - 100 primeiros números naturais@main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {]
+int itera_index
+for(itera_index = 0; itera_index < 100; itera_index++){
+    printf("%d\n", itera_index);
+}
+	return 0;
+}
