ef90abdbe037dc1c5ee65c0908234ae1d65bc5b2
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-06-03 18:00:06 +0000

    comandos para C em shell
    falta eu ver como ver a versão do linux

---
+++ comandos.tcl
@@ -0,0 +1,9 @@
+~/Desafios-Huxley$ ls
+Desafio-0  Desafio-1  Makefile  replit.nix
+~/Desafios-Huxley$ cd Desafio-1
+~/Desafios-Huxley/Desafio-1$ ls
+main.c
+~/Desafios-Huxley/Desafio-1$ clang main.c -o exe
+~/Desafios-Huxley/Desafio-1$ ls
+exe  main.c
+~/Desafios-Huxley/Desafio-1$ ./exe
